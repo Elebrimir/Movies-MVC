a9e822fd73b747476a9cfb9e5ad056061a416051
Pablo Cortés <cortesoltra@gmail.com>
2023-11-11 20:10:36 +0100

    He rectificat el arxiu per a que siga per a Vistes

--- routes/moviesView.js
+++ routes/moviesView.js
@@ -5,21 +5,32 @@ const express = require("express");
 let router = express.Router();
 
 const moviesController = require("../controllers/moviesView");
-const moviesIdController = require("../controllers/moviesViewId");
+const moviesIdController = require("../controllers/moviesView_Id");
+
+// Controlador base
 
 //GET /movies/llistar - Retorna la llista de pel·lícules
 router.get("/", moviesController.getMovies);
 
-//GET /movies/:id - Retorna una pel·lícula en concret
-router.get("/:id", moviesIdController.getMovie);
+//GET /movies/inserir - Mostra el Formulari per afegir pel·lícules
+router.get("/inserir", moviesController.addMoviesView);
 
-//POST /movies - Afegeix una pel·lícula a la llista
+//POST /movies/inserir - Afegeix una pel·lícula a la llista
 router.post("/inserir", moviesController.postMovie);
 
-//PATCH /movies/:id - Modificar una pel·lícula
-router.patch("/:id", moviesIdController.updateMovie);
 
-//DELETE /movies/:id - Eliminar una pel·lícula en concret
-router.delete("/:id", moviesIdController.deleteMovie);
+// Controlador que li passem el parametre _id al endpoint
+
+//GET /movies/:id - Mostra una pel·lícula en concret
+router.get("/:id", moviesIdController.getMovie);
+
+//GET /movies/:id - Modificar una pel·lícula
+router.get("/update/:id", moviesIdController.updateMovieView);
+
+//POST /movies/:id - Modificar una pel·lícula
+router.post("/update/:id", moviesIdController.updateMovie);
+
+//POST /movies/delete/:id - Eliminar una pel·lícula en concret
+router.post("/delete/:id", moviesIdController.deleteMovie);
 
 module.exports = router;
