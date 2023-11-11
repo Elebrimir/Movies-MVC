8fa7b1b816a622dbe2aaedc3797da8cdffe7767c
Pablo Cortés <cortesoltra@gmail.com>
2023-11-11 20:05:58 +0100

    afegits el cors,urlencoded,nunjucks

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const nunjucks = require("nunjucks");
+const cors = require("cors");
 
 const moviesRouterView = require("./routes/moviesView");
-const moviesRouterAPI = require("./routes/movies");
+const moviesRouterAPI = require("./routes/moviesAPI");
 
 const app = express();
 const port = 8080;
@@ -16,9 +17,12 @@ nunjucks.configure("views", { autoescape: true, express: app });
 
 app.set("view engine", "njk");
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/movies", moviesRouterView);
-app.use("/api/movies", moviesRouterAPI);
+app.use("/api/v1/movies", moviesRouterAPI);
 
 app.get("/", (req, res) => res.send("Hola Món!"));
 app.listen(port, () => console.log(`App escoltant en port ${port}!`));
