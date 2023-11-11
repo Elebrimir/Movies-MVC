612a775b7cde727bb57b131d2f4c6c6b62889f68
Pablo Cortés <cortesoltra@gmail.com>
2023-11-11 20:07:15 +0100

    Agregat lo de les versions de mongo

--- models/movies.js
+++ models/movies.js
@@ -2,30 +2,33 @@
 
 const mongoose = require("mongoose");
 
-let movieSchema = new mongoose.Schema({
-  id: String,
-  title: {
-    type: String,
-    required: true,
+let movieSchema = new mongoose.Schema(
+  {
+    id: String,
+    title: {
+      type: String,
+      required: true,
+    },
+    year: {
+      type: Number,
+      min: 1900,
+      max: 2024,
+    },
+    director: String,
+    duration: {
+      type: Number,
+      min: 1,
+    },
+    genre: [String],
+    rate: {
+      type: Number,
+      min: 0,
+      max: 10,
+      default: 5,
+    },
   },
-  year: {
-    type: Number,
-    min: 1900,
-    max: 2024,
-  },
-  director: String,
-  duration: {
-    type: Number,
-    min: 1,
-  },
-  genre: [String],
-  rate: {
-    type: Number,
-    min: 0,
-    max: 10,
-    default: 5,
-  },
-});
+  { versionKey: "__v" }
+);
 
 let Movie = mongoose.model("movie", movieSchema);
 
