e2ba5e7955e191b20c4352714bf143149b0fa79f
Pablo Cortés <cortesoltra@gmail.com>
2023-11-21 23:42:16 +0100

    DOTENV | Instalar npm dotenv

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const nunjucks = require("nunjucks");
 const cors = require("cors");
-const path = require('path');
-
+const path = require("path");
+const dotenv = require("dotenv").config();
 
 const moviesRouterView = require("./routes/moviesView");
 const moviesRouterAPI = require("./routes/moviesAPI");
@@ -27,10 +27,10 @@ app.use("/movies", moviesRouterView);
 app.use("/api/v1/movies", moviesRouterAPI);
 
 app.get("/", (req, res) => {
-    // Ruta al archivo web que quieres enviar
-    const filePath = path.join(__dirname, './views/home.njk');
+  // Ruta al archivo web que quieres enviar
+  const filePath = path.join(__dirname, "./views/home.njk");
 
-    // Enviar el archivo como respuesta
-    res.render(filePath);
-  });
+  // Enviar el archivo como respuesta
+  res.render(filePath);
+});
 app.listen(port, () => console.log(`App escoltant en port ${port}!`));
