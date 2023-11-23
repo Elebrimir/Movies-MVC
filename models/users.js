6a5f8cb93c7e66a1839dc5d468cd2f88b8c1acbf
Pablo Cortés <cortesoltra@gmail.com>
2023-11-23 14:22:45 +0100

    AFEGIR ROL A USERS | Afegir el component Rol al model Users

--- models/users.js
+++ models/users.js
@@ -12,6 +12,12 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  role: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Role",
+    },
+  ],
 });
 
 const User = mongoose.model("Users", userSchema);
