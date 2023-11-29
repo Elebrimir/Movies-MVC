27548eaebce639689bc6bb005adbd3d0e6c387b7
Pablo Cortés <cortesoltra@gmail.com>
2023-11-29 20:56:03 +0100

    SOLUCIONAT ERROR DE VALIDACIÓ | hi habia un error al validar ja que en els roles inferiors també hi ha que validar els rols superiors

--- middlewares/isGuest.js
+++ middlewares/isGuest.js
@@ -5,6 +5,9 @@ const Role = require("../models/roles");
 async function isGuest(req, res, next) {
   try {
     const guestRole = await Role.findOne({ roleName: "User" });
+    const userRole = await Role.findOne({ roleName: "User" });
+    const superUserRole = await Role.findOne({ roleName: "Superuser" });
+    const administratorRole = await Role.findOne({ roleName: "Administrator" });
 
     if (!guestRole) {
       return res.status(500).json({ message: "Error interno del servidor" });
@@ -12,7 +15,13 @@ async function isGuest(req, res, next) {
 
     // Comparar la ID del rol 'Guest' amb el valor del camp 'role' de l'usuari
 
-    if (req.user && req.user.role.toString() === guestRole._id.toString()) {
+    if (
+      (req.user && req.user.role.toString() === guestRole._id.toString()) ||
+      (req.user && req.user.role.toString() === userRole._id.toString()) ||
+      (req.user && req.user.role.toString() === superUserRole._id.toString()) ||
+      (req.user &&
+        req.user.role.toString() === administratorRole._id.toString())
+    ) {
       return next(); // Permitir acces
     } else {
       return res.status(403).json({ message: "Acceso no autorizado" });
--- middlewares/isSuperuser.js
+++ middlewares/isSuperuser.js
@@ -5,14 +5,19 @@ const Role = require("../models/roles");
 async function isSuperuser(req, res, next) {
   try {
     const superUserRole = await Role.findOne({ roleName: "Superuser" });
+    const administratorRole = await Role.findOne({ roleName: "Administrator" });
 
-    if (!superUserRole) {
+    if (!superUserRole || !administratorRole) {
       return res.status(500).json({ message: "Error interno del servidor" });
     }
 
     // Comparar la ID del rol 'SuperUser' amb el valor del camp 'role' de l'usuari
 
-    if (req.user && req.user.role.toString() === superUserRole._id.toString()) {
+    if (
+      (req.user && req.user.role.toString() === superUserRole._id.toString()) ||
+      (req.user &&
+        req.user.role.toString() === administratorRole._id.toString())
+    ) {
       return next(); // Permitir acces
     } else {
       return res.status(403).json({ message: "Acceso no autorizado" });
